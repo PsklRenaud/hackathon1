41763562945e6b10bd0d4a60db0db328e19f7f44
pascal renaud <wcs@pascal-renaud.com>
2018-10-30 19:32:17 +0100

    page acceuil terminé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PageAcceuilComponent } from './page-acceuil/page-acceuil.component';
+import { MenuComponent } from './menu/menu.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAcceuilComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule, 
+    BrowserAnimationsModule,
+
+    RouterModule.forRoot(
+      [
+
+      { path: "menu", component: MenuComponent, pathMatch:"full" },
+      { path: "page-acceuil", component: PageAcceuilComponent, pathMatch:"full" },
+      { path: "", redirectTo: "page-acceuil", pathMatch:"full" },
+      ],
+      
+      {useHash: true}
+      ),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
