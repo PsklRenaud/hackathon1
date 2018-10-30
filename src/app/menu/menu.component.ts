a0827575fc235d688e8eb75a1d2814d9a7b973e1
pascal renaud <wcs@pascal-renaud.com>
2018-10-30 22:17:24 +0100

    menu semi terminé

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,5 +11,28 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
   }
+  showMenu() {
+    const bigMenu = document.getElementById("bigMenu");
+    const logo = document.getElementById("logo");
+    if (bigMenu.classList.contains("hidden")) {
+      bigMenu.classList.remove("hidden");
+      bigMenu.classList.add("Onit");
+      logo.classList.add("Onit2");
+      logo.classList.add("logo")
+      } else {
+      bigMenu.classList.add("hidden");
+      bigMenu.classList.remove("Onit");
+      logo.classList.remove("Onit2");
+      logo.classList.remove("logo")
+      }
+    }
+  hideMenu() {
+    const bigMenu = document.getElementById("bigMenu");
+    const logo = document.getElementById("logo");
+    bigMenu.classList.add("hidden");
+    bigMenu.classList.remove("Onit");
+    logo.classList.remove("logo")
+    }
+  }
+  
 
-}
